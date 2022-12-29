8fff5e0172b221b3511a9fc1d7edbd9e70680cee
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-12-29 19:05:00 +0900

    feat: mainCont__product-special swiper js 연결

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 new Swiper(".mainCont__visual .swiper-container", {
   effect: "fade",
-  speed: 1000,
+  speed: 500,
+  autoplay: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+new Swiper(".mainCont__product-special .swiper-container", {
+  speed: 500,
   autoplay: true,
   loop: true,
   pagination: {
