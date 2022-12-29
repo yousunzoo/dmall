e4db92f5f50cceb403770ef51314772f1934819a
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-12-29 22:33:07 +0900

    refactor: swiper-container 클래스명 변경
    - .swiper
    - swiper-pagination 레이아웃 수정

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-new Swiper(".mainCont__visual .swiper-container", {
+new Swiper(".mainCont__visual .swiper", {
   effect: "fade",
   speed: 500,
   autoplay: true,
@@ -9,7 +9,7 @@ new Swiper(".mainCont__visual .swiper-container", {
   },
 });
 
-new Swiper(".mainCont__product-special .swiper-container", {
+new Swiper(".mainCont__product-special .swiper", {
   speed: 500,
   autoplay: true,
   loop: true,
