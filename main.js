03334e68ec81a2f9be488e88f111c5e0f62d20f3
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2022-12-29 18:25:21 +0900

    feat: mainVisual-swiper swiper js 연결

---
+++ main.js
@@ -0,0 +1,10 @@
+new Swiper(".mainCont__visual .swiper-container", {
+  effect: "fade",
+  speed: 1000,
+  autoplay: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
